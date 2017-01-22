f414fa227f173465870fb3ad367fec2fa8e19960
Kirill Zharenkov <extazy1701@gmail.com>
2017-01-22 11:30:15 +0200

    Behavioral patterns

---
+++ Behavioral patterns/Command.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<memory>
+
+class Receiver
+{
+public: 
+	void action() const
+	{
+		std::cout << "Reciever" << std::endl;
+	}
+};
+
+class Command
+{
+protected:
+	Receiver* reciever;
+public:
+	explicit Command(const Receiver& rhs): reciever(&const_cast<Receiver&>(rhs)){}
+
+	virtual void execute() const = 0;
+};
+
+
+class ConcreteCommand : public Command
+{
+public:
+	explicit ConcreteCommand(const Receiver& rhs): Command(rhs)
+	{}
+
+	void execute() const  override
+	{
+		reciever->action();
+	}
+};
+
+class Invoker
+{
+	Command* command;
+public:
+	Invoker(): command() {};
+
+	void store_command(const Command& rhs)
+	{
+		command = &const_cast<Command&>(rhs);
+	}
+
+	void execute_command() const
+	{
+		command->execute();
+	}
+};
+
+int main()
+{
+	Receiver reciever;
+	Invoker invoker;
+	std::shared_ptr<Command> command = std::make_shared<ConcreteCommand>(reciever);
+	invoker.store_command(*command);
+	invoker.execute_command();
+
+	system("pause");
+	return 0;
+}
