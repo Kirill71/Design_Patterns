2da8437adf2cfff7156cbdd1cd81a70772bedb1b
Kirill Zharenkov <extazy1701@gmail.com>
2017-01-25 16:40:50 +0200

    Behavioral patterns

---
+++ Behavioral patterns/State.cpp
@@ -0,0 +1,61 @@
+#include<memory>
+#include<iostream>
+class Context;
+
+class State
+{
+public:
+	virtual void handle(Context& context) const = 0;
+};
+
+class Context
+{
+	State* state_;
+public:
+	explicit Context(const State& state):state_(&const_cast<State&>(state)) {}
+	
+	const State& state() const { return *state_;}
+	State*& state() { return state_; }
+	void request()
+	{
+		state_->handle(*this);
+	}
+};
+
+class ConcreteStateA : public State
+{	
+public:
+	ConcreteStateA(){}
+	void handle(Context& context) const override;
+};
+
+class ConcreteStateB : public State
+{
+public:
+	ConcreteStateB() {}
+	void handle(Context& context) const  override
+	{
+		context.state() = &ConcreteStateA();
+		std::cout << "Concrete State A " << std::endl;
+	}
+};
+
+void ConcreteStateA::handle(Context& context) const 
+
+{
+	context.state() = &ConcreteStateB();
+	std::cout << "Concrete State B " << std::endl;
+}
+
+int main()
+{
+	ConcreteStateA state;
+	Context context(state);
+	context.request();
+	context.request();
+
+	system("pause");
+	return 0;
+}
+
+
