b6bbc914e77e2609272c5f0573b4b4ab868dac84
Kirill <extazy1701@gmail.com>
2017-01-26 12:08:34 +0200

    Behavioral patterns

--- Behavioral patterns/Iterator.cpp
+++ Behavioral patterns/Iterator.cpp
@@ -3,17 +3,20 @@
 #include<memory>
 #include<string>
 
+//If you'll change collection type, 
+//you should change methods ConcreteAggregate::add() & ConcreteAggregate::remove() & 
+//Aggregate::operator[] or delete it ConcreteAggregate .
 template< typename Type>
 using collection = std::vector<Type>;
 
 template< typename T>
 class Iterator
-{   
+{
 public:
 	virtual std::shared_ptr<T> first() const = 0;
 	virtual std::shared_ptr<T> next() = 0;
 	virtual bool isDone() = 0;
-	virtual const T& currentItem() const  = 0;
+	virtual const T& currentItem() const = 0;
 };
 
 template<typename T>
@@ -26,7 +29,6 @@ public:
 	virtual  T& operator[] (int index) = 0;
 };
 
-
 template<typename T>
 class ConcreteIterator : public Iterator<T>
 {
@@ -36,7 +38,6 @@ public:
 	ConcreteIterator() :aggregate() {}
 	explicit ConcreteIterator(const Aggregate<T>& _aggregate) :
 		aggregate{ &const_cast<Aggregate<T>&>(_aggregate) } {}
-
 
 	std::shared_ptr<T> first() const override
 	{
@@ -53,7 +54,7 @@ public:
 		return (*aggregate)[current];
 	}
 
-	bool isDone()
+	bool isDone() override
 	{
 		if (current < aggregate->count())
 			return false;
@@ -63,13 +64,12 @@ public:
 	}
 };
 
-
 template<typename T>
 class ConcreteAggregate : public Aggregate<T>
 {
 	collection<T> items;
 public:
-	ConcreteAggregate():items(4) {};
+	ConcreteAggregate() :items(4) {};
 	std::shared_ptr<Iterator<T>> createIterator()  const override
 	{
 		return std::make_shared<ConcreteIterator<T>>(*this);
@@ -88,20 +88,19 @@ public:
 			[index]
 		);
 	}
-	void insert(const T& val) 
+	void insert(const T& val)
 	{
 		items.push_back(val);
 	}
 	void remove(const T& val)
 	{
-		items.erase(std::find(items.cbegin(), items.cend(), val));
+		items.erase(std::find(items.cbegin(),items.cend(),val));
 	}
 };
 
-
-int main() 
+int main()
 {
-	std::unique_ptr<Aggregate<std::string>> a {std::make_unique<ConcreteAggregate<std::string>>()};
+	std::unique_ptr<Aggregate<std::string>> a{ std::make_unique<ConcreteAggregate<std::string>>() };
 	(*a)[0] = "Element A";
 	(*a)[1] = "Element B";
 	(*a)[2] = "Element C";
@@ -110,7 +109,7 @@ int main()
 	auto iter = a->createIterator();
 	for (auto e = iter->first(); !iter->isDone(); e = iter->next())
 		std::cout << *e << std::endl;
-	
+
 	system("Pause");
 	return 0;
 }
